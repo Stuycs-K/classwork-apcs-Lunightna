fc4e04bf8da70804e3f03accd903027fed77916f
vanna <vanna.lei123@gmail.com>
2024-09-29 20:52:35 -0400

    wrote returncopy function

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,8 +1,14 @@
 public class MyArrays{
   public static void main(String[] args) {
-    int[] ary = {1, 2, 3};
+    int[] ary = {1, 2, 3}; //test arrayToString
+    int [] ary1a = {};
+    int [] ary2a = {1};
+    int [] ary3a = {1, 2};
     System.out.println(arrayToString(ary));
+    System.out.println(returnCopy(ary));
+
   }
+
   public static String arrayToString(int[]ary){
     String newstr = "[";
       for (int i=0; i<ary.length;i++){
@@ -15,7 +21,16 @@ public class MyArrays{
       }
     return newstr + "]";
 }
+
   public static int[] returnCopy(int[]ary){
+    int[] newarr = new int[ary.length];
+    for(int i=0; i<ary.length;i++){
+      newarr[i] = ary[i];
+    }
+    return newarr;
+  }
+
+  public static int[] concatArray(int[]ary1,int[]ary2){
     return null;
   }
 }
