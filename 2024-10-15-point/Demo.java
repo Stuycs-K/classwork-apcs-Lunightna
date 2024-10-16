e2fcb6433db2b4181d1a2c87bfaeeedc6a2a6cee
Vanna Lei <vanna.lei123@gmail.com>
2024-10-16 10:00:37 -0400

    added more test cases

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -13,9 +13,15 @@ public class Demo{
     Point p1 = new Point(1,1);
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
+    Point p4 = new Point(-2,-5);
+    Point p5 = new Point(0,6);
     System.out.println( p3);
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
+    System.out.println( p4);
+    System.out.println( distance(p4,p5));
+    System.out.println( Point.distance(p5,p4));
+    System.out.println( p5.distanceTo(p4));
   }
 }
