5f822d080f529c6192e6eac57a67f7ad9e81cfec
Vanna Lei <vanna.lei123@gmail.com>
2024-11-07 09:47:52 -0500

    wrote animal constructor

--- 2024-11-07-Pets/Animal.java
+++ 2024-11-07-Pets/Animal.java
@@ -4,7 +4,9 @@ public class Animal {
   private String name;
 
   public Animal(String noise, int age, String name){
-    //initialize the values (hint use this.varname to clarify)
+    this.noise = noise;
+    this.age = age;
+    this.name = name;
   }
 
   //2.
