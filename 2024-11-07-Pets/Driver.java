4cd02efebf17ccb0e6d239f504bf501a3ce4c7a0
Vanna Lei <vanna.lei123@gmail.com>
2024-11-07 10:07:06 -0500

    overrided the speak() method and tested

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -2,5 +2,7 @@ public class Driver{
   public static void main(String[] args) {
     Animal a = new Animal("helloo", 20, "bob");
     a.speak();
+    Bird b = new Bird("helloo", 20, "bob", 10.0, "blue");
+    b.speak();
   }
 }
