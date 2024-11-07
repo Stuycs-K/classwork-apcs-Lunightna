84307104bf7c393aa228913688978bafd3811f8d
Vanna Lei <vanna.lei123@gmail.com>
2024-11-07 10:14:26 -0500

    commented what step 7 did for step 8

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,8 +1,10 @@
+// Overriding the getName() method changed the name to "The Mighty " + getName().
+
 public class Driver{
   public static void main(String[] args) {
-    Animal a = new Animal("helloo", 20, "bob");
+    Animal a = new Animal("helloo", 20, "Bob");
     a.speak();
-    Bird b = new Bird("helloo", 20, "bob", 10.0, "blue");
+    Bird b = new Bird("helloo", 20, "Bob", 10.0, "blue");
     b.speak();
   }
 }
