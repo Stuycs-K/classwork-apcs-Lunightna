788d67a904909355ab77be1337342af5a56f9363
vanna <vanna.lei123@gmail.com>
2024-11-15 00:33:33 -0500

    fixed ArrayList

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public class ArrayListPractice{
   public static ArrayList<String>createRandomArray (int size){
     ArrayList<String> arr = new ArrayList<>(size);
-    for (int i = 0; i < arr.size(); i++) {
-      arr.add("" + Math.random() * 10);
+    for (int i = 0; i < size; i++) {
+      arr.add("" + (int)(Math.random() * 10));
       if (arr.get(i).equals("0")) {
         arr.set(i, "");
       }
