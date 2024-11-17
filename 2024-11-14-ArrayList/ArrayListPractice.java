e646fa8d1cf8f70cf06124229a7e73d26008b1ad
vanna <vanna.lei123@gmail.com>
2024-11-17 16:58:38 -0500

    simplified mix lists using math.min and removed the conditionals and tested

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -35,33 +35,48 @@ public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String
   //a[0], b[0], a[1], b[1]...
   //If one list is longer than the other, just attach the remaining values to the end.
   ArrayList<String> arr = new ArrayList<String>(a.size()+b.size());
+  int min = Math.min(a.size(), b.size());
+  for(int i = 0; i<min; i++){
+    arr.add(a.get(i));
+    arr.add(b.get(i));
+    }
+  for(int i = min; i < a.size(); i++){
+    arr.add(a.get(i));
+  } 
+  for(int i = min; i < b.size(); i++){
+    arr.add(b.get(i));
+  } 
+  return arr;
+  }
+}
+
+/*
+Original/ less simplifed version
   if(a.size()>b.size()){
     for(int i = 0; i<b.size(); i++){
       arr.add(a.get(i));
       arr.add(b.get(i));
       }
-      for(int idx = b.size(); idx<a.size(); idx++){
-        arr.add(a.get(idx));
-      }
+    for(int idx = b.size(); idx<a.size(); idx++){
+      arr.add(a.get(idx));
+    }
+    return arr;
   }
   if(b.size()>a.size()){
     for(int i = 0; i<a.size(); i++){
       arr.add(a.get(i));
       arr.add(b.get(i));
       }
-      for(int idx = a.size(); idx<b.size(); idx++){
-        arr.add(a.get(idx));
-      }
+    for(int idx = a.size(); idx<b.size(); idx++){
+      arr.add(a.get(idx));
+    }
+    return arr;
   }
   else{
     for(int i = 0; i<a.size(); i++){
       arr.add(a.get(i));
       arr.add(b.get(i));
       }
+    return arr;
   }
-
-  return arr;
-
-  }
-
-}
+  */
--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -3,12 +3,18 @@ import java.util.ArrayList;
 public class Driver{
   public static void main(String[] args) {
     ArrayList<String> arr = ArrayListPractice.createRandomArray(10);
-    System.out.println(arr);
+    System.out.println("arr random array: " + arr);
     ArrayListPractice.replaceEmpty(arr);
-    System.out.println(arr);
-    System.out.println(ArrayListPractice.makeReversedList(arr));
+    System.out.println("Replace empty in arr: " + arr);
+    System.out.println("Reveresed arr: " + ArrayListPractice.makeReversedList(arr));
     ArrayList<String> arr1 = ArrayListPractice.createRandomArray(10);
-    System.out.println(arr1);
-    System.out.println(ArrayListPractice.mixLists(arr, arr1));
+    ArrayList<String> arr2 = ArrayListPractice.createRandomArray(5);
+    ArrayList<String> arr3 = ArrayListPractice.createRandomArray(12);
+    System.out.println("arr1: " + arr1);
+    System.out.println("arr2: " + arr2);
+    System.out.println("arr3: " + arr3);
+    System.out.println("Testing mixlist(arr, arr1): " + ArrayListPractice.mixLists(arr, arr1));
+    System.out.println("Testing mixlist(arr1, arr2): " + ArrayListPractice.mixLists(arr1, arr2));
+    System.out.println("Testing mixlist(arr3, arr3): " + ArrayListPractice.mixLists(arr2, arr3));
   }
 }
