cd363d9f6e2fc918ae28d939f4e7536c118b04fe
vanna <vanna.lei123@gmail.com>
2024-11-18 23:26:46 -0500

    removed structure for day 1 part2

--- 2024-11-18-advent2016/advent2016.java
+++ 2024-11-18-advent2016/advent2016.java
@@ -45,43 +45,4 @@ public class advent2016{
       return 0;
     }
   }
-
-  public static Integer taxicaB(String filename){
-    File file = new File(filename);
-    try {
-      Scanner input = new Scanner(file);
-      input.useDelimiter(",\\s*");
-      int cordx = 0;
-      int cordy = 0;
-      int direction = 0;
-      while(input.hasNext()){
-        String a = input.next();
-        if(a.charAt(0)==('R')){
-          direction++;
-        }
-        if(a.charAt(0)==('L')){
-          direction--;
-        }
-        direction = (direction + 4) % 4;
-        int unit = Integer.parseInt(a.substring(1));
-        if(direction==0){
-          cordy += unit;
-        }
-        if(direction==1){
-          cordx += unit;
-        }
-        if(direction==2){
-          cordy -= unit;
-        }
-        if(direction==3){
-          cordx -= unit;
-        }
-      }
-      return Math.abs(cordx) + Math.abs(cordy);
-    }
-    catch(FileNotFoundException ex){
-      System.out.println("File not found");
-      return 0;
-    }
-  }
 }
