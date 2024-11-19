9c428b478b44ddda66f5f64f39b771db0e630f45
vanna <vanna.lei123@gmail.com>
2024-11-18 23:25:30 -0500

    day 1 part 1

--- 2024-11-18-advent2016/advent2016.java
+++ 2024-11-18-advent2016/advent2016.java
@@ -4,44 +4,84 @@ import java.io.FileNotFoundException;
 
 public class advent2016{
   public static void main(String[] args) {
-    System.out.println(taxicab("inputTri.txt"));
-  }
-public static String taxicab(String filename){
-  File file = new File(filename);
-  try {
-    Scanner input = new Scanner(file);
-    int cordx = 0;
-    int cordy = 0;
-    int direction = 0;
-    while(input.hasNext()){
-      String a = input.next();
-      if(a[0]=="R"){
-        direction++;
-      }
-      if(a[0]=="L"){
-        direction--;
-      }
-      direction %= 4;
-      int unit = Integer.parseInt(a.substring(1));
-      if(direction==0){
-        cordy += unit;
-      }
-      if(direction==1){
-        cordx += unit;
-      }
-      if(direction==2){
-        cordy -= unit;
-      }
-      if(direction==3){
-        cordx -= unit;
-      }
+    System.out.println(taxicabA("inputTri.txt"));
+    System.out.println(taxicabB("inputTri.txt"));
     }
+  public static Integer taxicabA(String filename){
+    File file = new File(filename);
+    try {
+      Scanner input = new Scanner(file);
+      input.useDelimiter(",\\s*");
+      int cordx = 0;
+      int cordy = 0;
+      int direction = 0;
+      while(input.hasNext()){
+        String a = input.next();
+        if(a.charAt(0)==('R')){
+          direction++;
+        }
+        if(a.charAt(0)==('L')){
+          direction--;
+        }
+        direction = (direction + 4) % 4;
+        int unit = Integer.parseInt(a.substring(1));
+        if(direction==0){
+          cordy += unit;
+        }
+        if(direction==1){
+          cordx += unit;
+        }
+        if(direction==2){
+          cordy -= unit;
+        }
+        if(direction==3){
+          cordx -= unit;
+        }
+      }
+      return Math.abs(cordx) + Math.abs(cordy);
+    }
+    catch(FileNotFoundException ex){
+      System.out.println("File not found");
+      return 0;
+    }
+  }
 
-  }
-  catch(FileNotFoundException ex){
-    System.out.println("File not found");
-    return "";
-  }
-  return "";
+  public static Integer taxicaB(String filename){
+    File file = new File(filename);
+    try {
+      Scanner input = new Scanner(file);
+      input.useDelimiter(",\\s*");
+      int cordx = 0;
+      int cordy = 0;
+      int direction = 0;
+      while(input.hasNext()){
+        String a = input.next();
+        if(a.charAt(0)==('R')){
+          direction++;
+        }
+        if(a.charAt(0)==('L')){
+          direction--;
+        }
+        direction = (direction + 4) % 4;
+        int unit = Integer.parseInt(a.substring(1));
+        if(direction==0){
+          cordy += unit;
+        }
+        if(direction==1){
+          cordx += unit;
+        }
+        if(direction==2){
+          cordy -= unit;
+        }
+        if(direction==3){
+          cordx -= unit;
+        }
+      }
+      return Math.abs(cordx) + Math.abs(cordy);
+    }
+    catch(FileNotFoundException ex){
+      System.out.println("File not found");
+      return 0;
+    }
   }
 }
