16933560e605e4c288c698e35e9a4f2ff5cd91f0
vanna <vanna.lei123@gmail.com>
2024-11-25 00:56:44 -0500

    wrote day 2 part 2

--- 2024-11-18-advent2016/day2.java
+++ 2024-11-18-advent2016/day2.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 public class day2{
   public static void main(String[] args) {
     System.out.println(bathroomA("bathroom.txt"));
+    System.out.println(bathroomB("bathroom.txt"));
     }
 
   public static String bathroomA(String filename){
@@ -44,6 +45,56 @@ public class day2{
           }
         }
         code = code + "" + keypad[x][y];
+      }
+      return code;
+    }
+    catch(FileNotFoundException ex){
+      System.out.println("File not found");
+    }
+    return null;
+  }
+    public static String bathroomB(String filename){
+    File file = new File(filename);
+    try{
+      Scanner input = new Scanner(file);
+      int x = 0;
+      int y = 3;
+      String code = "";
+      char[][] keypad = {
+      {' ', ' ', '1', ' ', ' '},
+      {' ', '2', '3', '4', ' '},
+      {'5', '6', '7', '8', '9'},
+      {' ', 'A', 'B', 'C', ' '},
+      {' ', ' ', 'D', ' ', ' '}};
+      while(input.hasNextLine()){
+        String a = input.nextLine();
+        for(int i = 0; i<a.length();i++){
+          if(a.charAt(i)=='R'){
+            x++;
+            if(((y==0 || y==4) && x>2) ||((y==1 || y==3) && x>3) || (y==2 && x>4)){
+              x--;
+            }
+          }
+          if(a.charAt(i)=='L'){
+            x--;
+            if(((y==0 || y==4) && x<2) ||((y==1 || y==3) && x<1) || (y==2 && x<0)){
+              x++;
+            }
+          }
+          if(a.charAt(i)=='U'){
+            y--;
+            if(((x==0 || x==4) && y<2) ||((x==1 || x==3) && y<1) || (x==2 && y<0)){
+              y++;
+            }
+          }
+          if(a.charAt(i)=='D'){
+            y++;
+            if(((x==0 || x==4) && y>2) ||((x==1 || x==3) && y>3) || (x==2 && y>4)){
+              y--;
+            }
+          }
+        }
+        code = code + "" + keypad[y][x];
       }
       return code;
     }
