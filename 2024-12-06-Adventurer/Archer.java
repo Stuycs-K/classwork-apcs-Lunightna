b8851d2c409774e47f171fcd1321144bef68a5b1
vanna <vanna.lei123@gmail.com>
2024-12-08 23:41:01 -0500

    made private variables and the custom consumable resource

---
+++ 2024-12-06-Adventurer/Archer.java
@@ -0,0 +1,33 @@
+public class Archer extends Adventurer{
+    private int mana;
+    private int arrowCount;
+    private int bowDurability;
+
+    public Archer(String name, int hp){
+        super(name, hp);
+        mana = 100;
+        arrowCount = 10;
+        bowDurability = 100;
+    }
+
+    public String getSpecialName(){
+        return "Infinity Hood";
+    }
+
+    public int getSpecial(){
+        return mana;
+    }
+
+    public void setSpecial(int n){
+        if(mana>0){
+            mana = n;
+        }
+        else{
+            mana = 0;
+        }
+    }
+
+    public int getSpecialMax(){
+        return Math.pow(10, 7);
+    }
+}
