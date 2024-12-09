fe496ffadaae9cd1ad08e25aefc0ba55995d2d38
vanna <vanna.lei123@gmail.com>
2024-12-08 23:59:33 -0500

    added mana capacity, fixed limits, created attacks and support

--- 2024-12-06-Adventurer/Archer.java
+++ 2024-12-06-Adventurer/Archer.java
@@ -1,11 +1,13 @@
 public class Archer extends Adventurer{
     private int mana;
+    private int manaCapacity;
     private int arrowCount;
     private int bowDurability;
 
     public Archer(String name, int hp){
         super(name, hp);
         mana = 100;
+        manaCapacity = 100;
         arrowCount = 10;
         bowDurability = 100;
     }
@@ -21,6 +23,9 @@ public class Archer extends Adventurer{
     public void setSpecial(int n){
         if(mana>0){
             mana = n;
+            if(mana>manaCapacity){
+                mana = manaCapacity;
+            }
         }
         else{
             mana = 0;
@@ -28,6 +33,26 @@ public class Archer extends Adventurer{
     }
 
     public int getSpecialMax(){
-        return 1000000;
+        return manaCapacity;
     }
+
+    public String attack(Adventurer other){
+        int damage = Math.sqrt(mana/arrowCount);
+        arrowCount--;
+        bowDurability -= 10;
+        return getName() + "dealt " + damage + " on " + other.getName();
+    }
+
+    public String support(Adventurer other){
+        int heal = (mana/10);
+    }
+
+    public String support(){
+        int healme = (mana/10);
+        setHP(getHP() + healme);
+        bowDurability += 10;
+        return "healed" + healme + ", increased bow durability by 10";
+    }
+
+
 }
