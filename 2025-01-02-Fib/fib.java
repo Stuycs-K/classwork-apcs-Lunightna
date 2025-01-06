8d6615d4c15e63fbc1436503c1c348c6c72813f9
vanna <vanna.lei123@gmail.com>
2025-01-06 00:12:22 -0500

    wrote makeNoDoubleLetterWords

--- 2025-01-02-Fib/fib.java
+++ 2025-01-02-Fib/fib.java
@@ -1,7 +1,7 @@
 public class fib{
   public static void main(String[] args) {
     //System.out.println(fibseq(46)); //46 is largest under 10 secs and largest n that fits in an int
-    makeWords(3, "", "ab");
+    makeNoDoubleLetterWords(3, "", "123");
   }
 
   public static int fibseq(int n){
@@ -21,6 +21,20 @@ public class fib{
       for(int i = 0; i<alphabet.length();i++){
         String letter = String.valueOf(alphabet.charAt(i));
         makeWords(remainingLetters-1, result +letter, alphabet);
+      }
+    }
+  }
+
+  public static void makeNoDoubleLetterWords(int remainingLetters, String result, String alphabet){
+    if(remainingLetters==0){
+      System.out.println(result);
+    }
+    else{
+      for(int i=0; i<alphabet.length();i++){
+        String letter = String.valueOf(alphabet.charAt(i));
+        if(result.isEmpty() || !result.endsWith(letter)){
+          makeNoDoubleLetterWords(remainingLetters-1, result +letter, alphabet);
+        }
       }
     }
   }
